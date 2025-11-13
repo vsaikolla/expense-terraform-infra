d336e43786181c6891681e6d35f854976a66b58b
sainath <kollavenkatasai999@gmail.com>
2025-11-13 18:15:04 -0500

    07-backend

--- 07-backend/backend.sh
+++ 07-backend/backend.sh
@@ -1,3 +1,6 @@
 #/bin/bash
-dnf install ansible -y
+component= $1
+environment= $2
+sudo dnf install ansible -y
 pip3.9 install botocore boto3
+ansible-pull -i localhost, -U https://github.com/vsaikolla/ansible-expense-roles-tf-infra.git main.yaml -e component=$component -e env=$environment
