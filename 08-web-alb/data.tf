b43d71a3ede84a509b3fbf4eec04d625b15d5ff4
sainath <kollavenkatasai999@gmail.com>
2025-11-13 21:20:38 -0500

    ..

--- 08-web-alb/data.tf
+++ 08-web-alb/data.tf
@@ -6,3 +6,6 @@ data "aws_ssm_parameter" "public_subnet_ids" {
     name = "/${var.project_name}/${var.environment}/public_subnet_ids"
 }
 
+data "aws_ssm_parameter" "web_alb_listener_arn_https" {
+    name = "/${var.project_name}/${var.environment}/web_alb_listener_arn_https"
+}
