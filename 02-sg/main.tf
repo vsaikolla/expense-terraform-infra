073003ca5f8f916883197074757c90eb9c36f115
sainath <kollavenkatasai999@gmail.com>
2025-11-13 20:09:18 -0500

    ..

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -130,13 +130,13 @@ resource "aws_security_group_rule" "backend_vpn_http" {
   security_group_id = module.backend.sg_id
 }
 
-## Frontend accepting connections from Public,Bastion 
-resource "aws_security_group_rule" "frontend_public" {
+## Frontend accepting connections from Bastion, web-alb & vpn 
+resource "aws_security_group_rule" "frontend_web_alb" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  source_security_group_id = module.web_alb.sg_id 
   security_group_id = module.frontend.sg_id
 }
 resource "aws_security_group_rule" "frontend_bastion" {
@@ -144,7 +144,15 @@ resource "aws_security_group_rule" "frontend_bastion" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  source_security_group_id = module.bastion.sg_id
+  source_security_group_id = module.bastion.sg_id 
+  security_group_id = module.frontend.sg_id
+}
+resource "aws_security_group_rule" "frontend_vpn" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.vpn.sg_id 
   security_group_id = module.frontend.sg_id
 }
 
@@ -158,7 +166,7 @@ resource "aws_security_group_rule" "bastion_public" {
   security_group_id = module.bastion.sg_id 
 }
 
-# app-alb accepting connections from vpn
+# app-alb accepting connections from vpn,bastion & frontend
 resource "aws_security_group_rule" "app_alb_vpn" {
   type              = "ingress"
   from_port         = 80
@@ -166,4 +174,38 @@ resource "aws_security_group_rule" "app_alb_vpn" {
   protocol          = "tcp"
   source_security_group_id = module.vpn.sg_id
   security_group_id = module.app_alb.sg_id 
+}
+resource "aws_security_group_rule" "app_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id 
+  security_group_id = module.app_alb.sg_id
+}
+resource "aws_security_group_rule" "app_alb_frontend" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.frontend.sg_id 
+  security_group_id = module.app_alb.sg_id
+}
+
+# web-alb accepting connections from  public & public_https
+resource "aws_security_group_rule" "web_alb_public" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.web_alb.sg_id
+}
+resource "aws_security_group_rule" "web_alb_public_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.web_alb.sg_id
 }
