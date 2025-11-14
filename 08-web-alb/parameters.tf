623b07940874410f2b73ab1168e143c06457b02f
sainath <kollavenkatasai999@gmail.com>
2025-11-13 19:51:18 -0500

    web-alb

--- 08-web-alb/parameters.tf
+++ 08-web-alb/parameters.tf
@@ -1,5 +1,5 @@
-resource "aws-aws_ssm_parameter" "app_alb_listener_arn" {
-    name = "/${var.project_name}/${var.environment}/app_alb_listener_arn"
+resource "aws-aws_ssm_parameter" "web_alb_listener_arn" {
+    name = "/${var.project_name}/${var.environment}/web_alb_listener_arn"
     type = "String"
     value = aws_lb_listener.http.arn
 }
